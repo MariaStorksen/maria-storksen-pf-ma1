a3bdd6753838ab0b7f453ef33ca18d40a8b17e6d
Maria Storksen <maria.storksen@gmail.com>
2020-09-24 15:15:28 +0200

    Delivery ma1, programming foundations

---
+++ script.js
@@ -0,0 +1,31 @@
+// question 1
+
+// var firstVar;
+
+// question 2
+
+// var name; name = "Maria";
+
+// question 3
+
+// var secondVar; secondVar = 5;
+
+// question 4
+
+// var division; division = 20 / 5;
+
+// question 5
+
+// console.log("frog");
+
+// question 6
+
+// var orderHasShipped; orderHasShipped = true;
+
+// question 7
+
+// if (orderHasShipped === true) {console.log("The order has been shipped");} else {console.log("The order has not been shipped");}
+
+// question 8
+
+// var i; for (i = 1; i <= 9; i++) {console.log(i);}
