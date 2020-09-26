94e9b56f57d4c8d307b5fa481b674ef5adaa9444
Maria Storksen <maria.storksen@gmail.com>
2020-09-26 11:24:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,46 @@
 // question 1
 
-// var firstVar;
+var firstVar;
+
 
 // question 2
 
-// var name; name = "Maria";
+var name = "Maria";
+
 
 // question 3
 
-// var secondVar; secondVar = 5;
+var secondVar = 5;
+
 
 // question 4
 
-// var division; division = 20 / 5;
+var division = 20 / 5;
+
 
 // question 5
 
-// console.log("frog");
+console.log(typeof "frog");
+
 
 // question 6
 
-// var orderHasShipped; orderHasShipped = true;
+var orderHasShipped = true;
+
 
 // question 7
 
-// if (orderHasShipped === true) {console.log("The order has been shipped");} else {console.log("The order has not been shipped");}
+if (orderHasShipped === true) {
+  console.log("The order has been shipped");
+} 
+else {
+  console.log("The order has not been shipped");
+}
+
 
 // question 8
 
-// var i; for (i = 1; i <= 9; i++) {console.log(i);}
+var i; 
+for (i = 1; i <= 9; i++) {
+  console.log(i);
+}
